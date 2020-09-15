7c0b3216d7320310b3bd094b5c94d946ee81dc1b
Namito Yokota <namitoyokota@Namitos-MacBook-Air.local>
2020-09-15 14:49:47 -0500

    deliverables list

--- src/pages/Deliverables.js
+++ src/pages/Deliverables.js
@@ -2,14 +2,22 @@
 
 import React, { Component } from "react";
 import { Container } from "reactstrap";
+import { ListGroup, ListGroupItem } from "reactstrap";
 import styled from "styled-components";
+
+import PitchPresentation from "../content/pitch.pdf";
 
 const Title = styled.h1`
   text-align: center;
-  padding-top: 3em;
-  &:hover {
-    text-decoration: none;
-  }
+  padding: 1em;
+  font-size: 4em;
+  font-weight: bold;
+`;
+
+const Box = styled.div`
+  text-align: center;
+  margin: 0 auto;
+  width: 20em;
 `;
 
 class Deliverables extends Component {
@@ -17,6 +25,26 @@ class Deliverables extends Component {
     return (
       <Container>
         <Title>Deliverables</Title>
+        <Box>
+          <ListGroup>
+            <ListGroupItem
+              tag="a"
+              href="https://github.com/CrimsonMinistry/CrimsonMinistry"
+              target="_blank"
+              action
+            >
+              Source Code
+            </ListGroupItem>
+            <ListGroupItem
+              tag="a"
+              href={PitchPresentation}
+              download="PitchPresentation.pdf"
+              action
+            >
+              Project Pitch Slides
+            </ListGroupItem>
+          </ListGroup>
+        </Box>
       </Container>
     );
   }
