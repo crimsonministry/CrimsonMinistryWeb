567a75d3b026ff0d4ef08847492304aa5bbba28a
wabeaumont <wabeaumont@crimson.ua.edu>
2020-09-22 00:40:47 -0500

    Revised Home page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,26 +4,33 @@ import React, { Component } from "react";
 import { Container } from "reactstrap";
 import styled from "styled-components";
 
+const Background = {
+  background: "#ffffff"
+}
+
 const Title = styled.h1`
   text-align: center;
-  padding-top: 2em;
   font-size: 4em;
   font-weight: bold;
-  background-color: #dcdcdc;
 `;
 
 const Subtitle = styled.p`
-  text-align: center;
+  text-align: left;
   font-size: 1.75em;
-  background-color: #dcdcdc;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  width: 750px;
+  margin: auto;
 `;
 
 class Main extends Component {
   render() {
     return (
       <Container>
-        <Title>Crimson Ministry</Title>
-        <Subtitle>This app is designed to connect students from different churches at the University of Alabama to one another. Typically, the events for different churches are insulated within the members of that particular faith. This platform allows students to organize events with one another and promote interaction between different groups. This allows for greater participation and understanding for the various ministries present on-campus. Students are also able to post prayer requests promote awareness for causes in need. We aim to foster a greater sense of community beyond one's personal church.</Subtitle>
+	<div style = {Background}>
+          <Title>Crimson Ministry</Title>
+          <Subtitle>This app is designed to connect students from different churches at the University of Alabama with one another. Typically, the events for different churches are insulated within the members of that particular faith. This platform allows students to organize events with one another and promote interaction between different groups. This allows for greater participation and understanding for the various ministries present on-campus. Students are also able to post prayer requests promote awareness for causes in need. We aim to foster a greater sense of community beyond one's personal church.</Subtitle>
+        </div>
       </Container>
     );
   }
